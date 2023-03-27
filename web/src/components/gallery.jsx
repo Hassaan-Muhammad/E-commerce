e5dbbcb8d79a51dc8d78e787dceb0fe225615d9d
hassaan <hassaans299@gmail.com>
2023-03-27 22:23:18 +0500

    post

--- web/src/components/gallery.jsx
+++ web/src/components/gallery.jsx
@@ -1,7 +1,12 @@
+import './gallery.css';
 
 function Gallery() {
 
-    return <h1>This is gallery</h1>
+    return <h1 className="heading">A media query consists of a media type and can contain one or more expressions, which resolve to either true or false. The result of the query is true if the specified media type matches the type of device the document is being displayed on and all expressions in the media query are true. 
+
+    </h1>
+
+
 
 }
 
