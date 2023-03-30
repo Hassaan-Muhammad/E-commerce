1e729049cd7655f56cbcba3d75c216a2e3da3b4b
hassaan <hassaans299@gmail.com>
2023-03-30 15:03:45 +0500

    styling done on login and signup

--- web/src/components/login.jsx
+++ web/src/components/login.jsx
@@ -48,52 +48,109 @@ function Login() {
 
 
     return (
-        
-        <div className="loginDiv"> 
-            <h4 className="headingLogin">This is Login page</h4>
 
-            {state.text}
-
-            <form onSubmit={loginHandler} className="loginForm">
+        <div>
 
 
-                <textarea
-                    className="TextField"
-                    id="email"
-                    label="Email"
-                    variant="outlined"
-                    type="email"
-                    name="username"
-                    placeholder="email"
-                    autoComplete="username"
-                    onChange={(e) => { setEmail(e.target.value) }}
-                />
+
+            {/* <div className="loginDiv">
+                <h4 className="headingLogin">This is Login page</h4>
+
+                {state.text}
+
+                <form onSubmit={loginHandler} className="loginForm">
 
 
+                    <textarea
+                        className="TextField"
+                        id="email"
+                        label="Email"
+                        variant="outlined"
+                        type="email"
+                        name="username"
+                        placeholder="email"
+                        autoComplete="username"
+                        onChange={(e) => { setEmail(e.target.value) }}
+                    />
+
+
+                    <br />
+
+                    <textarea
+                        className="TextField"
+                        id="password"
+                        label="Password"
+                        variant="outlined"
+                        type="password"
+                        name="current-password"
+                        autoComplete="current-password"
+                        placeholder="password"
+                        onChange={(e) => { setPassword(e.target.value) }}
+                    />
+
+                    <br />
+
+                </form>
                 <br />
 
-                <textarea
-                    className="TextField"
-                    id="password"
-                    label="Password"
-                    variant="outlined"
-                    type="password"
-                    name="current-password"
-                    autoComplete="current-password"
-                    placeholder="password"
-                    onChange={(e) => { setPassword(e.target.value) }}
-                />
 
-                <br />
-                <button className="loginbutton" type="submit">Login</button>
 
-            </form>
-            <br/>
-            
-            <Link className="forgotButton" to={`/forget-password`}>Forget Password</Link>
-            
-            <p>{result}</p>
-        
+            </div> */}
+
+
+
+            {/* fake */}
+            <div class="login-box">
+                <h2>Login</h2>
+                {state.text}
+                <form onSubmit={loginHandler}>
+                    <div class="user-box">
+                        <input
+                            required=""
+
+                            id="email"
+                            type="email"
+                            name="username"
+                            placeholder="email"
+                            autoComplete="username"
+                            onChange={(e) => { setEmail(e.target.value) }} />
+                        <label>Username</label>
+                    </div>
+
+                    <div class="user-box">
+                        <input
+                            required=""
+
+                            id="password"
+                            type="password"
+                            name="current-password"
+                            autoComplete="current-password"
+                            placeholder="password"
+                            onChange={(e) => { setPassword(e.target.value) }}
+                        />
+                        <label>Password</label>
+                    </div>
+
+                    <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <button className="loginbutton" type="submit">Login</button>
+                    </a>
+                </form>
+
+                <br/>
+                
+                <Link className="forgotButton" to={`/forget-password`}>Forget Password</Link>
+
+                <p>{result}</p>
+
+            </div>
+
+
+
+
         </div>
     )
 }
--- web/src/components/signup.jsx
+++ web/src/components/signup.jsx
@@ -45,25 +45,69 @@ function Signup() {
 
 
     return (
+        <div>
 
-        <div className="signupDiv">
-            <h4 className="Heading">This is Signup page</h4>
 
-            <form className="signupForm" onSubmit={signupHandler}>
-                <label> Name:</label>
-                <input  className="inputfield1" type="text" name="name" placeholder="Enter your name" onChange={(e) => { setName(e.target.value) }} />
-                <br />
-                Email: <input className="inputfield2" type="email" name="username" autoComplete="username" placeholder="email" onChange={(e) => { setEmail(e.target.value) }} />
-                <br />
-                Password: <input className="inputfield3" type="password" name="new-password" autoComplete="new-password" placeholder="password" onChange={(e) => { setPassword(e.target.value) }} />
-                <br />
-                Confirm Password: <input className="inputfield4" type="password" name="new-password" autoComplete="new-password" placeholder="confirm password" />
-                <br />
-                <button className="signupButton" type="submit">Signup</button>
-            </form>
-            <p>{result}</p>
+            {/* <div className="signupDiv">
+                <h4 className="Heading">This is Signup page</h4>
+
+                <form className="signupForm" onSubmit={signupHandler}>
+                    <label> Name:</label>
+                    <input className="inputfield1" type="text" name="name" placeholder="Enter your name" onChange={(e) => { setName(e.target.value) }} />
+                    <br />
+                    Email: <input className="inputfield2" type="email" name="username" autoComplete="username" placeholder="email" onChange={(e) => { setEmail(e.target.value) }} />
+                    <br />
+                    Password: <input className="inputfield3" type="password" name="new-password" autoComplete="new-password" placeholder="password" onChange={(e) => { setPassword(e.target.value) }} />
+                    <br />
+                    Confirm Password: <input className="inputfield4" type="password" name="new-password" autoComplete="new-password" placeholder="confirm password" />
+                    <br />
+                    <button className="signupButton" type="submit">Signup</button>
+                </form>
+                <p>{result}</p>
+            </div> */}
+
+            {/* fake */}
+
+            <div class="login-box">
+                <h2>This is Signup page</h2>
+                <form onSubmit={signupHandler}>
+
+                    <div class="user-box">
+                        <input  type="text" required="" name="name" placeholder="Enter your name" onChange={(e) => { setName(e.target.value) }} />
+                        <label>Username</label>
+                    </div>
+
+                    <div class="user-box">
+                        <input  type="email" name="username" autoComplete="username" placeholder="email" onChange={(e) => { setEmail(e.target.value) }} />
+                        <label>Email</label>
+                    </div>
+
+                    <div class="user-box">
+                        <input  type="password" name="new-password" autoComplete="new-password" placeholder="password" onChange={(e) => { setPassword(e.target.value) }} />
+                        <label>Password</label>
+                    </div>
+
+                    <div class="user-box">
+                        <input  type="password" name="new-password" autoComplete="new-password" placeholder="confirm password" />
+                        <label>Confirm Password</label>
+                    </div>
+
+
+                    <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <button className="signupButton" type="submit">Signup</button>
+                    </a>
+                </form>
+
+                <p>{result}</p>
+            </div>
+
+
+
         </div>
-
     )
 }
 
